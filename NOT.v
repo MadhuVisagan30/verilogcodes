4522124e652b1f31019f05f20a26da59fb77805e
Madhu Visagan H T <visaganhtm300507@gmail.com>
2026-05-15 14:00:37 +0530

    inverter gate
    Replaced 'not' gate with an assignment operator for output.

--- NOT.v
+++ NOT.v
@@ -1,4 +1,14 @@
+// my solution
 module top_module( input in, output out );
     not(out,in);
 
+endmodule
+// hdlbits solution
+module top_module(
+	input in,
+	output out
+);
+	
+	assign out = ~in;
+	
 endmodule
