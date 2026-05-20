ac89bd9752600dbed7d2ffe37aa99c8a0fdc500e
Madhu Visagan H T <visaganhtm300507@gmail.com>
2026-05-20 12:37:25 +0530

    Implement 32-bit adder using two 16-bit adders
    Added top_module to instantiate two 16-bit adders for 32-bit addition.

--- module_fadd.v
+++ module_fadd.v
@@ -37,3 +37,47 @@ module add1 (
     assign cout = (a & b) | (a & cin) | (b & cin);
 
 endmodule
+//(or)
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    wire c1,c2;
+    add16 prithiv(a[15:0],b[15:0],1'b0,sum[15:0],c1);
+    add16 prithiv2(a[31:16],b[31:16],c1,sum[31:16],c2);
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+    always @(*)begin
+    case({a,b,cin})
+        3'b000:begin
+             sum=1'b0;
+            cout=1'b0;end
+        3'b001:begin
+              sum=1'b1;
+            cout=1'b0;end
+        3'b010:begin
+            sum=1'b1;
+            cout=1'b0;end
+        3'b011:begin
+            sum=1'b0;
+            cout=1'b1;end
+        3'b100:begin
+           sum=1'b1;
+            cout=1'b0;end
+        3'b101:begin
+          sum=1'b0;
+            cout=1'b1;end
+        3'b110:begin
+     sum=1'b0;
+            cout=1'b1;end
+        3'b111:begin
+           sum=1'b1;
+            cout=1'b1;end
+    endcase
+    end
+
+endmodule
+
